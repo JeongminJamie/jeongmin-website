750490912e542e3b65dac4bb9706a151af0c525e
JeongminJamie <sksrhcalsu@gmail.com>
2023-02-22 15:59:04 -0500

    matching name and menu on the same line

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,29 @@
-import * as React from "react"
-import { Link } from "gatsby"
+// import * as React from "react"
+// import { Link } from "gatsby"
 
-import Navbar from "./navbar"
+// import Navbar from "./navbar"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
-      display: `flex`,
-      alignItems: `center`,
-      justifyContent: `space-between`,
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      Jeongmin Choi
-    </Link>
-    <Navbar />
-  </header>
-)
+// const Header = ({ siteTitle }) => (
+//   <header
+//     style={{
+//       margin: `0 auto`,
+//       padding: `var(--space-4) var(--size-gutter)`,
+//       display: `flex`,
+//       alignItems: `center`,
+//       justifyContent: `space-between`,
+//     }}
+//   >
+//     <Link
+//       to="/"
+//       style={{
+//         fontSize: `var(--font-sm)`,
+//         textDecoration: `none`,
+//       }}
+//     >
+//       Jeongmin Choi
+//     </Link>
+//     <Navbar />
+//   </header>
+// )
 
-export default Header
+// export default Header
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,22 +2,33 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 const Navbar = () => (
-  <nav className="navbar">
-    <Link
-      to="/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      <span>Jeongmin Choi</span>
-    </Link>
-    <ul>
-      <li>HOME</li>
-      <li>ABOUT</li>
-      <li>PROJECTS</li>
-      <li>CONTACT</li>
-    </ul>
+  <nav
+    className="navbar"
+    style={{
+      margin: `0 auto`,
+      padding: `var(--space-4) var(--size-gutter)`,
+      display: `flex`,
+      justifyContent: `space-between`,
+    }}
+  >
+    <div>
+      <Link to="/">
+        <span>Jeongmin Choi</span>
+      </Link>
+    </div>
+    <div>
+      <ul
+        style={{
+          display: `flex`,
+          listStyle: `none`,
+        }}
+      >
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>PROJECTS</li>
+        <li>CONTACT</li>
+      </ul>
+    </div>
   </nav>
 )
 
