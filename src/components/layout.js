128252c1388f50ec2d8e5d7995a199edb3c87e17
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-06 20:11:59 -0500

    working on the social icons section

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@
  */
 
 import * as React from "react"
+import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Navbar from "./navbar"
@@ -26,6 +27,25 @@ const Layout = ({ children }) => {
     <>
       <Navbar />
       <div>
+        <section className="heroIcons">
+          <div className="hero_icon">
+            <Link to="https://www.linkedin.com/in/jeongmin-choi-43508a20a/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M14.447 3.027a.75.75 0 01.527.92l-4.5 16.5a.75.75 0 01-1.448-.394l4.5-16.5a.75.75 0 01.921-.526zM16.72 6.22a.75.75 0 011.06 0l5.25 5.25a.75.75 0 010 1.06l-5.25 5.25a.75.75 0 11-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 010-1.06zm-9.44 0a.75.75 0 010 1.06L2.56 12l4.72 4.72a.75.75 0 11-1.06 1.06L.97 12.53a.75.75 0 010-1.06l5.25-5.25a.75.75 0 011.06 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </Link>{" "}
+          </div>
+          <div className="hero_icon"></div>
+        </section>
         <div
           className="indexContent"
           style={{
