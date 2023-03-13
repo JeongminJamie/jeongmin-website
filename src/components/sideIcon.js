9127a1cdcaedca40286321995a2334f65c50db3d
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-13 17:26:06 -0400

    sideicons' background color will be changed when hovered

--- src/components/sideIcon.js
+++ src/components/sideIcon.js
@@ -6,7 +6,7 @@ import "./cssFiles/sideIcon.css"
 const SideIcon = () => (
   <section className="heroIcons">
     <div className="github_icon">
-      <Link to="https://github.com/JeongminJamie">
+      <Link to="https://github.com/JeongminJamie" className="iconLink">
         <img
           src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg"
           alt="Github profile"
@@ -19,7 +19,10 @@ const SideIcon = () => (
       </Link>
     </div>
     <div className="linkedin_icon">
-      <Link to="https://www.linkedin.com/in/jeongmin-choi-43508a20a/">
+      <Link
+        to="https://www.linkedin.com/in/jeongmin-choi-43508a20a/"
+        className="iconLink"
+      >
         <img
           src="https://d33wubrfki0l68.cloudfront.net/d8e6e1e636531e28274a1b8b6d947b817f6145bd/d42d3/assets/svg/linkedin-dark.svg"
           alt="Linkedin profile"
@@ -32,7 +35,10 @@ const SideIcon = () => (
       </Link>
     </div>
     <div className="resume_icon">
-      <Link to="https://resume.creddle.io/resume/2an8hfmoz4n">
+      <Link
+        to="https://resume.creddle.io/resume/2an8hfmoz4n"
+        className="iconLink"
+      >
         <img
           src="https://d33wubrfki0l68.cloudfront.net/e978c8491d93813aa00b3dc80b9abfb0d64a9086/3c187/assets/svg/book-dark.svg"
           alt="Resume profile"
