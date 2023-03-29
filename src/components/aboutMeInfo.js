02418d6e0880c1a7c2f74183caa94f8d9750736b
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-29 17:11:34 -0400

    working on introduction of about page styling

--- src/components/aboutMeInfo.js
+++ src/components/aboutMeInfo.js
@@ -6,27 +6,29 @@ import "./cssFiles/aboutMeInfo.css"
 const AboutMeInfo = () => (
   <div className="aboutMeInfo">
     <div className="introductionContainer">
-      <h3 className="introductionText">Who I am</h3>
-      <p className="introContent">
-        I'm a <span>Full-stack web developer</span> always looking for the
-        improvement points of website for high quality product. You can check my{" "}
-        <span>projects</span> with various stacks clicking right{" "}
-        <span>Here.</span>
-      </p>
-      <p className="introContent">
-        I'm willing to share my <span>experiences</span> what I've learned in{" "}
-        <span>Web Development.</span> Also, I'm eager to learn and adopt to fast
-        transitions of programming.
-      </p>
-      <p className="introContent">
-        I'm open to <span>job opportunities</span> in web development industry.
-        If you are interested in my tech skills, feel free to contact me through
-        the <span>Contact</span> button below.
-      </p>
-      <div classsName="contactButtonContainer">
-        <Link to="/contact">
-          <button className="contactButton">CONTACT</button>
-        </Link>
+      <h3 className="introductionText">Who am I?</h3>
+      <div className="introContentContainer">
+        <p className="introContent">
+          I'm a <span>Full-stack web developer</span> always looking for the
+          improvement points of website for high quality product. You can check
+          my <span>projects</span> with various stacks clicking right{" "}
+          <span>Here.</span>
+        </p>
+        <p className="introContent">
+          I'm willing to share my <span>experiences</span> what I've learned in{" "}
+          <span>Web Development.</span> Also, I'm eager to learn and adopt to
+          fast transitions of programming.
+        </p>
+        <p className="introContent">
+          I'm open to <span>job opportunities</span> in web development
+          industry. If you are interested in my tech skills, feel free to
+          contact me through the <span>Contact</span> button below.
+        </p>
+        <div classsName="contactButtonContainer">
+          <Link to="/contact">
+            <button className="contactButton">CONTACT</button>
+          </Link>
+        </div>
       </div>
     </div>
   </div>
