9cfd4eabb732d736132fadf663ed611a1047b5a7
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-29 16:49:58 -0400

    added introduction content in about section

--- src/components/aboutMeInfo.js
+++ src/components/aboutMeInfo.js
@@ -4,10 +4,22 @@ import "./cssFiles/aboutMeInfo.css"
 
 const AboutMeInfo = () => (
   <div className="aboutMeInfo">
-    <div className="introdusctionContainer">
-      <h3 className="introductionText">Introduction</h3>
-      <p className="introContent1">
-        More information about me, and current skills of programming here
+    <div className="introductionContainer">
+      <h3 className="introductionText">Who I am</h3>
+      <p className="introContent">
+        I'm a Full-stack web developer always looking for the improvement points
+        of website for high quality product. You can check my projects with
+        various stacks clicking right Here.
+      </p>
+      <p className="introContent">
+        I'm willing to share my experiences what I've learned in Web
+        Development. Also, I'm eager to learn and adopt to fast transitions of
+        programming.
+      </p>
+      <p className="introContent">
+        I'm open to job opportunities in web development industry. If you are
+        interested in my tech skills, feel free to contact me through the
+        Contact section.
       </p>
     </div>
   </div>
