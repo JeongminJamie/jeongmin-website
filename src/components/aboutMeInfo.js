ece716d9f19540937d9e2fefbce179cf405cdd17
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-30 16:02:53 -0400

    created skills boxes for about me page

--- src/components/aboutMeInfo.js
+++ src/components/aboutMeInfo.js
@@ -31,6 +31,14 @@ const AboutMeInfo = () => (
         </div>
       </div>
     </div>
+    <div className="aboutMeSkillContainer">
+      <div className="skill">1</div>
+      <div className="skill">2</div>
+      <div className="skill">3</div>
+      <div className="skill">4</div>
+      <div className="skill">5</div>
+      <div className="skill">6</div>
+    </div>
   </div>
 )
 
