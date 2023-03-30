1eeb665a5714fbae7e9041b30cfb2001a32ac38c
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-30 15:38:36 -0400

    fixed contact page background image

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -29,54 +29,52 @@ const Contact = () => {
       )
   }
   return (
-    <body className="wholeContact">
-      <Layout>
-        <div className="contactContainer">
-          <div className="contact">
-            <h2 className="contactMeText">Contact Me</h2>
-            <p className="contactContent">
-              Feel free to contact me with the form below
-            </p>
-          </div>
-          <section className="formContainer">
-            <form ref={form} onSubmit={sendEmail} className="emailForm">
-              <label>Name</label>
-              <input
-                className="nameInput"
-                type="text"
-                placeholder="Enter Your Name"
-                name="user_name"
-                required
-              />
-              <label>Email</label>
-              <input
-                className="emailInput"
-                type="email"
-                placeholder="Enter Your Email"
-                name="user_email"
-                required
-              />
-              <label>Message</label>
-              <textarea
-                className="messageInput"
-                type="text"
-                placeholder="Enter Your Message"
-                name="user_message"
-                required
-                cols="30"
-                rows="10"
-                style={{
-                  resize: "none",
-                }}
-              />
-              <button type="submit" className="submitBtn">
-                Submit
-              </button>
-            </form>
-          </section>
+    <Layout>
+      <div className="contactContainer">
+        <div className="contact">
+          <h2 className="contactMeText">Contact Me</h2>
+          <p className="contactContent">
+            Feel free to contact me with the form below
+          </p>
         </div>
-      </Layout>
-    </body>
+        <section className="formContainer">
+          <form ref={form} onSubmit={sendEmail} className="emailForm">
+            <label>Name</label>
+            <input
+              className="nameInput"
+              type="text"
+              placeholder="Enter Your Name"
+              name="user_name"
+              required
+            />
+            <label>Email</label>
+            <input
+              className="emailInput"
+              type="email"
+              placeholder="Enter Your Email"
+              name="user_email"
+              required
+            />
+            <label>Message</label>
+            <textarea
+              className="messageInput"
+              type="text"
+              placeholder="Enter Your Message"
+              name="user_message"
+              required
+              cols="30"
+              rows="10"
+              style={{
+                resize: "none",
+              }}
+            />
+            <button type="submit" className="submitBtn">
+              Submit
+            </button>
+          </form>
+        </section>
+      </div>
+    </Layout>
   )
 }
 
