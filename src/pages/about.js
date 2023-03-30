a7493e091cc96c0a896877f78d6354d0ebb83179
JeongminJamie <sksrhcalsu@gmail.com>
2023-03-30 14:09:51 -0400

    changed background color for about and contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,18 @@ import * as React from "react"
 import Layout from "../components/layout"
 import AboutMe from "../components/aboutMe"
 import AboutMeInfo from "../components/aboutMeInfo"
+
+import "../components/cssFiles/about.css"
+
 import Seo from "../components/seo"
 
 const About = () => (
-  <Layout>
-    <AboutMe />
-    <AboutMeInfo />
-  </Layout>
+  <body className="wholeAbout">
+    <Layout>
+      <AboutMe />
+      <AboutMeInfo />
+    </Layout>
+  </body>
 )
 
 export const Head = () => <Seo title="About" />
