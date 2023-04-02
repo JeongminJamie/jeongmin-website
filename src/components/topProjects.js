7968812b73c194baf6a274242e6462ae8a106814
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-02 16:10:03 -0400

    created topProjects and proejectsInfo files for projects page

---
+++ src/components/topProjects.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import "./cssFiles/topProjects.css"
+
+const TopProjects = () => (
+  <div className="projects">
+    <h2 className="projectsText">PROJECTS</h2>
+    <p className="projectsContent">
+      More information about me, and current skills of programming here
+    </p>
+  </div>
+)
+
+export default TopProjects
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,15 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import TopProjects from "../components/topProjects"
+import ProjectsInfo from "../components/projectsInfo"
+
 import Seo from "../components/seo"
 
 const Projects = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <TopProjects />
+    <ProjectsInfo />
   </Layout>
 )
 
