5ff38a415c86d767bdf5d7131dcd7e89d0f207a7
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-04 15:29:31 -0400

    working to change image in the real website on index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `src/images/myFace.jpeg`,
       },
     },
     `gatsby-transformer-sharp`,
