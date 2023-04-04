5852380c091f3b787fe82a43ff5a3edf0e2af81e
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-04 16:05:06 -0400

    changed the magin of header

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import "./cssFiles/header.css"
-// import face from "../images/myFace.jpeg"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Header = () => (
