92d2a1af2f8da0878bb60a9f083a5bb9b6e34470
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-05 17:31:28 -0400

    comment out the plugin setting of gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
