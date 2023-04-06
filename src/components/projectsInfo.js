34285d4b287ca02350ecc2f0802a33e9584a8b45
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-05 21:41:19 -0400

    added the freame of the project contents

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -3,9 +3,19 @@ import * as React from "react"
 import "./cssFiles/projectsInfo.css"
 
 const ProjectsInfo = () => (
-  <div className="projects">
-    <h2 className="projectsText">PROJECTS</h2>
-    <p className="projectsContent">projects section</p>
+  <div className="projectsInfo">
+    <div className="project">
+      <div className="title">Talkie</div>
+      <div className="description"></div>
+    </div>
+    <div className="project">
+      <div className="title">Don Don</div>
+      <div className="description"></div>
+    </div>
+    <div className="project">
+      <div className="title">Interview Scheduler</div>{" "}
+      <div className="description"></div>
+    </div>
   </div>
 )
 
