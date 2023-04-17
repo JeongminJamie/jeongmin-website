7d77fd071c0b291863400a1345f869a3b5c76c78
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-17 15:45:28 -0400

    added projects images into images folder

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -5,14 +5,17 @@ import "./cssFiles/projectsInfo.css"
 const ProjectsInfo = () => (
   <div className="projectsInfo">
     <div className="project">
+      <div className="talkieImg"></div>
       <div className="title">Talkie</div>
       <div className="description"></div>
     </div>
     <div className="project">
+    <div className="dondonImg"></div>
       <div className="title">Don Don</div>
       <div className="description"></div>
     </div>
     <div className="project">
+    <div className="schedulerImg"></div>
       <div className="title">Interview Scheduler</div>{" "}
       <div className="description"></div>
     </div>
