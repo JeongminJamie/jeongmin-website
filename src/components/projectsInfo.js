e2e29d066c13d76dd64cad1f6849e7b18a36e80e
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-17 17:33:58 -0400

    added projects container

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -4,26 +4,28 @@ import "./cssFiles/projectsInfo.css"
 
 const ProjectsInfo = () => (
   <div className="projectsInfo">
-    <div className="project">
-      <StaticImage
-        className="talkieImg"
-        src="../images/Talkie2.png"
-        objectFit="cover"
-        width={420}
-        height={250}
-      />
-      <div className="title">Talkie</div>
-      <div className="description"></div>
-    </div>
-    <div className="project">
-      <div className="dondonImg"></div>
-      <div className="title">Don Don</div>
-      <div className="description"></div>
-    </div>
-    <div className="project">
-      <div className="schedulerImg"></div>
-      <div className="title">Interview Scheduler</div>{" "}
-      <div className="description"></div>
+    <div className="projectsContainer">
+      <div className="project">
+        <StaticImage
+          className="talkieImg"
+          src="../images/Talkie2.png"
+          objectFit="cover"
+          width={420}
+          height={250}
+        />
+        <div className="title">Talkie</div>
+        <div className="description"></div>
+      </div>
+      <div className="project">
+        <div className="dondonImg"></div>
+        <div className="title">Don Don</div>
+        <div className="description"></div>
+      </div>
+      <div className="project">
+        <div className="schedulerImg"></div>
+        <div className="title">Interview Scheduler</div>{" "}
+        <div className="description"></div>
+      </div>
     </div>
   </div>
 )
