10498c1fe790607fecb1af555fef8fb93bac4828
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-17 19:40:04 -0400

    working on project descriptions on projects page

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -14,7 +14,11 @@ const ProjectsInfo = () => (
       />
       <div className="projectContent">
         <div className="title">Talkie</div>
-        <div className="description"></div>
+        <div className="description">
+          <p>
+            Technologies: React, Express, PostgreSQL, Media Recorder API, AWS S3
+          </p>
+        </div>
       </div>
     </div>
     <div className="project">
@@ -25,8 +29,12 @@ const ProjectsInfo = () => (
         width={420}
         height={250}
       />
-      <div className="title">Don Don</div>
-      <div className="description"></div>
+      <div className="projectContent">
+        <div className="title">Don Don</div>
+        <div className="description">
+          <p>Technologies: Express, Node, HTML, SASS, jQuery, Ajax</p>
+        </div>
+      </div>
     </div>
     <div className="project">
       <StaticImage
@@ -36,8 +44,12 @@ const ProjectsInfo = () => (
         width={420}
         height={250}
       />
-      <div className="title">Interview Scheduler</div>{" "}
-      <div className="description"></div>
+      <div className="projectContent">
+        <div className="title">Interview Scheduler</div>{" "}
+        <div className="description">
+          <p>Technologies: React</p>
+        </div>
+      </div>
     </div>
   </div>
 )
