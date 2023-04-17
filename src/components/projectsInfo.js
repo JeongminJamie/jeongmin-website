0cf9e05b998e1216205c1243b15a85f24fe2e763
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-17 17:06:34 -0400

    wokring on talkie static image for projects part

--- src/components/projectsInfo.js
+++ src/components/projectsInfo.js
@@ -1,21 +1,27 @@
 import * as React from "react"
-
+import { StaticImage } from "gatsby-plugin-image"
 import "./cssFiles/projectsInfo.css"
 
 const ProjectsInfo = () => (
   <div className="projectsInfo">
     <div className="project">
-      <div className="talkieImg"></div>
+      <StaticImage
+        src="../images/Talkie2.png"
+        objectFit="cover"
+        width={420}
+        height={250}
+        className="talkieImg"
+      />
       <div className="title">Talkie</div>
       <div className="description"></div>
     </div>
     <div className="project">
-    <div className="dondonImg"></div>
+      <div className="dondonImg"></div>
       <div className="title">Don Don</div>
       <div className="description"></div>
     </div>
     <div className="project">
-    <div className="schedulerImg"></div>
+      <div className="schedulerImg"></div>
       <div className="title">Interview Scheduler</div>{" "}
       <div className="description"></div>
     </div>
