38b8ed4d593e5a5d292d7d26e99cff1b66ae5827
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-25 19:33:19 -0400

    fixed website config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: ``,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Jeongmin Choi`,
+    description: `Jeongmin's personal website`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
