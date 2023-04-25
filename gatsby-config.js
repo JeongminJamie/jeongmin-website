e74b1d0e8a79554af24c7ddcf840e12e37b3da6d
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-25 19:35:37 -0400

    finished working on config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jeongmin website`,
+        short_name: `Jamie`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
