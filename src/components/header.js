7bad5b6c0faff58e6208e36996d19910aecf7536
JeongminJamie <sksrhcalsu@gmail.com>
2023-04-25 14:16:12 -0400

    added the useEffect to figure out the window size for styling of 600

--- src/components/header.js
+++ src/components/header.js
@@ -1,60 +1,75 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import "./cssFiles/header.css"
 
-const Header = () => (
-  <div className="header_main">
-    <div>
-      <Link
-        to="/"
-        style={{
-          textDecoration: `none`,
-        }}
-      >
-        <div className="faceLogo_name">
-          <div className="faceLogo_img"></div>
-          <span className="myName">Jeongmin Choi</span>
-        </div>
-      </Link>
-    </div>
-    <div className="header_nav">
-      <ul className="nav">
+const Header = () => {
+
+  // figuring out the window size
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      const ismobile = window.innerWidth
+      if (ismobile < 600) {
+        setIsMobile(true)
+      }
+    })
+  }, [isMobile])
+
+  return (
+    <div className="header_main">
+      <div>
         <Link
           to="/"
           style={{
             textDecoration: `none`,
           }}
         >
-          <li>HOME</li>
+          <div className="faceLogo_name">
+            <div className="faceLogo_img"></div>
+            <span className="myName">Jeongmin Choi</span>
+          </div>
         </Link>
-        <Link
-          to="/about"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          <li>ABOUT</li>
-        </Link>
-        <Link
-          to="/projects"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          <li>PROJECTS</li>
-        </Link>
-        <Link
-          to="/contact"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          <li>CONTACT</li>
-        </Link>
-      </ul>
+      </div>
+      <div className="header_nav">
+        <ul className="nav">
+          <Link
+            to="/"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <li>HOME</li>
+          </Link>
+          <Link
+            to="/about"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <li>ABOUT</li>
+          </Link>
+          <Link
+            to="/projects"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <li>PROJECTS</li>
+          </Link>
+          <Link
+            to="/contact"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            <li>CONTACT</li>
+          </Link>
+        </ul>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
